973f2af0daf16c24bb0f18bf2d529d759155deb3
Franklin Vale <franklin18@ru.is>
2018-09-04 11:41:10 +0000

    Algorithm written in python

--- max_int.py
+++ max_int.py
@@ -1,3 +1,16 @@
+max_int = 0
+num_int = 0
+while num_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int > max_int:
+        max_int = num_int
+
+print("The maximum is", max_int)    # Do not change this line
+
+
+
+
+
 #Take input from user
 #check to see if input is a negative number
 #if number is negative, stop taking inputs.
